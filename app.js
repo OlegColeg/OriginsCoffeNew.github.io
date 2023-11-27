dc90658873595dab27c9c500eec856c4f33821f4
OlegColeg <ooale47@gmail.com>
2023-11-27 10:44:47 +0200

    Fix hamburger menu functionality

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const btn = document.getElementById('btnb');
-const orar = document.getElementById('orar');
-
-btn.addEventListener('click', function() {
-  orar.classList.remove('orar-hidden'); // Elimină clasa care ascunde blocul
-
-  const timpAfisare = 5000; // timpul în milisecunde (în acest caz, 5 secunde)
-
-  setTimeout(function() {
-    orar.classList.add('orar-hidden'); // Adaugă înapoi clasa pentru a ascunde blocul
-  }, timpAfisare);
+let i= 0;
+document.getElementById('hamb').addEventListener('click', function() {
+  if (i == 0) {
+    document.getElementById('hamblist').style.display = 'flex';
+    i++;
+  }
+  else {
+    document.getElementById('hamblist').style.display = 'none';
+    i--;
+  }
+  console.log(i);
 });
